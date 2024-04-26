1b88d175d798c45ea8345d51fb944bca225183c9
suba-mahes <insu041831@gmail.com>
2024-04-26 18:39:29 +0530

    added routers

--- issue_display/app.js
+++ issue_display/app.js
@@ -4,23 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var jiraApiRouter = require('./routes/jira_API');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/issue-display', jiraApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,13 +22,18 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  
+  const err_json = {
+    "error_status" : err.status || 500,
+    "error_message" : err.message
+  }
+  
+  res.format({
+    "application/json"(){
+      res.status(err.status || 500);
+      res.json(err_json);
+    }
+  })
 });
 
 module.exports = app;
--- issue_display/routes/issue_display.js
+++ issue_display/routes/issue_display.js
@@ -3,7 +3,13 @@ const jira_api_controller = require("../controllers/issue_display");
 
 var router = express.Router();
 
-router.get("/welcome_using_JIRA_API", jira_api_controller.welcome_using_JIRA_API);
-router.get("/welcome_using_JIRA_client", jira_api_controller.welcome_using_JIRA_client);
+router.get(
+  "/welcome-using-JIRA_API",
+  jira_api_controller.welcome_using_JIRA_API
+);
+router.get(
+  "/welcome-using-JIRA_client",
+  jira_api_controller.welcome_using_JIRA_client
+);
 
 module.exports = router;
