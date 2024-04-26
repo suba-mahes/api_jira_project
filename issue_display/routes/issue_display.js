30d20bdaf497fb9c1f0e271475349142d9bc7108
suba-mahes <insu041831@gmail.com>
2024-04-26 18:20:16 +0530

    added changes

--- issue_display/routes/issue_display.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-const jira_api_controller = require("../controllers/issue_display");
-
-var router = express.Router();
-
-router.get("/welcome", jira_api_controller.welcome);
-
-module.exports = router;
